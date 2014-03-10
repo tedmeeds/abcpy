20d17cc6985d4f54fca514755250ffbe1f327cca
Ted Meeds <tmeeds@gmail.com>
2014-03-10 17:09:21 +0100

    added gaussian process surrogate

---
+++ surrogates/gps.py
@@ -0,0 +1,6 @@
+from progapy.gps.product_gaussian_process import ProductGaussianProcess
+from progapy.gps.basic_regression import BasicRegressionGaussianProcess
+
+class GaussianProcessSurrogate(object):
+  def __init__( self, params ):
+    pass 
