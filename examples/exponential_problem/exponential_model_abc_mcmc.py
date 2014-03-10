fixed bug in covariance, linspace u's, recomputing model

examples/exponential_problem/exponential_model_abc_mcmc.py
@@ -17,13 +17,13 @@ problem_params["beta"]            = 0.1
 problem_params["theta_star"]      = 0.1
 problem_params["N"]               = 500  # how many observations we draw per simulation
 problem_params["seed"]            = 0
-problem_params["q_stddev"]        = 0.25
+problem_params["q_stddev"]        = 0.01
 problem = Problem( problem_params, force_init = True )
 
 
 # since we are running abc_rejection, use a distance epsilon state
 state_params = {}
-state_params["S"]                          = 20
+state_params["S"]                          = 2
 state_params["obs_statistics"]             = problem.get_obs_statistics()
 state_params["theta_prior_rand_func"]      = problem.theta_prior_rand
 state_params["theta_prior_logpdf_func"]    = problem.theta_prior_logpdf
@@ -34,19 +34,20 @@ state_params["statistics_function"]        = problem.statistics_function
 state_params["log_kernel_func"]            = log_gaussian_kernel
 state_params["is_marginal"]                = False
 state_params["epsilon"]                    = 0.0
+#state_params["hierarchy_type"]      = "jeffreys"
+#state_params["hierarchy_type"]      = "jeffreys"
 
 
 
 model_params = {}
 # adaptive-SL params
-model_params["xi"]            = 0.4
-model_params["M"]             = 100
-model_params["deltaS"]        = 5
-model_params["max_nbr_tries"] = 20
-
+model_params["xi"]            = 0.1
+model_params["M"]             = 10
+model_params["deltaS"]        = 20
+model_params["max_nbr_tries"] = 10
 model = Model( model_params)
 
-nbr_samples = 10000
+nbr_samples = 1500
 #epsilon     = 0.5
 theta0 = max(np.array([1e-3]), problem.theta_prior_rand() )
 print "INIT THETA = ",theta0

models/adaptive_synthetic_likelihood_model.py
@@ -22,8 +22,8 @@ def unconditional_metropolis_hastings_error( alphas, tau ):
   # alphas:  random samples of acceptance probabiltiies
   # tau:     threshold, typically the empirical median of alphas
   err = 0.0
-  #u_stream = np.linspace( 0, 1, 1000 )
-  u_stream = np.random.rand( 100 )
+  u_stream = np.linspace( 0, 1, 100 )
+  #u_stream = np.random.rand( 100 )
   
   # "integrate" over u draws -- NB this is pretty inefficient way of doing it
   for u in u_stream:
@@ -70,21 +70,24 @@ class AdaptiveSyntheticLikelihoodModel( BaseMetropolisHastingsModel ):
       current_logliks  = self.current_state.loglikelihood_rand( self.M )
     
       self.log_accs = self.log_acceptance_offset + proposed_logliks - current_logliks
-      self.accs     = np.exp(self.log_accs)
+      
       
       I = pp.find( self.log_accs > 0 )
       self.log_accs[I] = 0
-      
+      self.accs     = np.exp(self.log_accs)
       self.error = self.metropolis_hastings_error( self.accs, u )
       #print "MH error = ", self.error
       nbr_tries += 1
       
       if nbr_tries < self.max_nbr_tries:
         if self.error > self.xi:
-          print nbr_tries,"error > xi: ",self.error
+          print "\t",nbr_tries, self.median, "  ","error > xi: ",self.error, self.proposed_state.mu_stats, self.proposed_state.n_stats, self.current_state.mu_stats, self.current_state.n_stats
           self.acquire_points()
-    if nbr_tries > 1:
-      print "    final error > xi: ",self.error
+    #if nbr_tries > 1:
+      
+    #print "final ",nbr_tries, self.median, "  ","error > xi: ",self.error, self.proposed_state.mu_stats, self.proposed_state.n_stats, self.current_state.mu_stats, self.current_state.n_stats
+          
+      #print "    final error > xi: ",self.error
     
         
     # Metropolis-Hastings acceptance log-probability and probability

problems/exponential.py
@@ -17,8 +17,10 @@ class ExponentialProblem( BaseProblem ):
     # proposal params (LogNormal)
     if params.has_key("q_stddev"):
       self.proposal_std    = params["q_stddev"]
-    self.proposal_rand   = lognormal_rand
-    self.proposal_logpdf = lognormal_logpdf
+    #self.proposal_rand   = lognormal_rand
+    #self.proposal_logpdf = lognormal_logpdf
+    self.proposal_rand   = positive_normal_rand
+    self.proposal_logpdf = normal_logpdf
     
     # "true" parameter setting
     self.theta_star = params["theta_star"]
@@ -44,9 +46,9 @@ class ExponentialProblem( BaseProblem ):
     self.observations   = self.simulation_function( self.theta_star )
     self.obs_statistics = self.statistics_function( self.observations )
     
-    self.min_range = 0.05
-    self.max_range = 0.15
-    self.range = (self.min_range,self.max_range)
+    self.min_range           = 0.05
+    self.max_range           = 0.15
+    self.range               = (self.min_range,self.max_range)
     self.fine_bin_width      = 0.0001
     self.coarse_bin_width    = 0.001
     
@@ -60,12 +62,12 @@ class ExponentialProblem( BaseProblem ):
     self.posterior_mode = (self.N + self.alpha)/(self.observations.sum() + self.beta)
     
     self.true_posterior_logpdf_func = gen_gamma_logpdf(self.alpha+self.N,self.beta+self.observations.sum())
-    self.true_posterior_cdf_func = gen_gamma_cdf(self.alpha+self.N,self.beta+self.observations.sum())
+    self.true_posterior_cdf_func    = gen_gamma_cdf(self.alpha+self.N,self.beta+self.observations.sum())
     
     self.posterior_bars_range = self.coarse_theta_range[:-1] + 0.5*self.coarse_bin_width
-    self.posterior_cdf = self.true_posterior_cdf_func( self.coarse_theta_range )
-    self.posterior_bars = (self.posterior_cdf[1:] - self.posterior_cdf[:-1])/self.coarse_bin_width
-    self.posterior_cdf_bins = self.posterior_cdf[1:] - self.posterior_cdf[:-1]
+    self.posterior_cdf        = self.true_posterior_cdf_func( self.coarse_theta_range )
+    self.posterior_bars       = (self.posterior_cdf[1:] - self.posterior_cdf[:-1])/self.coarse_bin_width
+    self.posterior_cdf_bins   = self.posterior_cdf[1:] - self.posterior_cdf[:-1]
     # done initialization
     self.initialized = True
     
@@ -98,10 +100,11 @@ class ExponentialProblem( BaseProblem ):
     return gamma_logprob( theta, self.alpha, self.beta ) # 1/beta cause of how python implements
       
   def theta_proposal_rand( self, theta ):
-    return self.proposal_rand( np.log(theta), self.proposal_std )
+    return self.proposal_rand( theta, self.proposal_std )
     
   def theta_proposal_logpdf( self, to_theta, from_theta ):
-    return self.proposal_logpdf( to_theta, np.log(from_theta), self.proposal_std )
+    return self.proposal_logpdf( to_theta, from_theta, self.proposal_std )
+    #return self.proposal_logpdf( to_theta, np.log(from_theta), self.proposal_std )
       
   # take samples/staistics etc and "view" this particular problem
   def view_results( self, states_object, burnin = 1 ):
@@ -175,6 +178,8 @@ class ExponentialProblem( BaseProblem ):
     pp.ylabel( "err")
     pp.grid('on')
     #pdb.set_trace()
-    print "ERROR  ",bin_errors_1d( self.coarse_theta_range, self.posterior_cdf_bins, thetas )
+    print "ERROR    ",bin_errors_1d( self.coarse_theta_range, self.posterior_cdf_bins, thetas )
+    print "ACC RATE ", states_object.acceptance_rate()
+    print "SIM      ", total_sims
     # return handle to figure for further manipulation
     return f

states/synthetic_likelihood_model.py
@@ -1,5 +1,5 @@
 from abcpy.state import BaseState
-from abcpy.helpers import logsumexp, log_pdf_full_mvn, gaussian_logpdf
+from abcpy.helpers import logsumexp, log_pdf_full_mvn, gaussian_logpdf, inv_wishart_rnd, wishart_rnd
 
 import numpy as np
 import scipy as sp
@@ -29,6 +29,10 @@ class SyntheticLikelihoodModelState( BaseState ):
     self.simulation_function   = params["simulation_function"]
     self.statistics_function   = params["statistics_function"]
     
+    self.hierarchy             = "just_gaussian"
+    if params.has_key("hierarchy_type"):
+      self.hierarchy = params["hierarchy_type"]
+      
     # kernel and its epsilon
     #self.log_kernel_func  = params["log_kernel_func"]
     self.epsilon          = params["epsilon"]
@@ -58,19 +62,26 @@ class SyntheticLikelihoodModelState( BaseState ):
   def acquire( self, nbr_points ):
     thetas, sim_outs, stats = self.run_sim_and_stats( nbr_points )
   
+    #pdb.set_trace()
     if len(self.stats) == 0:
       self.stats      = stats
       self.sim_ouputs = sim_outs
     else:
       self.stats      = np.vstack( (self.stats, stats) )
       self.sim_ouputs = np.vstack( (self.sim_ouputs, sim_outs) )
+    
+    self.update_model()
       
       
   def update_model( self ): 
     self.statistics   = np.mean( self.stats, 0 )
     self.mu_stats     = self.statistics
-    self.cov_stats    = np.cov( self.stats.T )
-    self.cov_mu_stats = self.cov_stats / self.S
+    self.n_stats      = len(self.stats)
+    dif = self.stats - self.mu_stats
+    self.sum_sq_stats = np.dot( dif.T, dif )
+    #self.cov_stats    = np.cov( self.stats.T, ddof = self.n_stats-1 )
+    self.cov_stats    = self.sum_sq_stats / (self.n_stats-1 )
+    self.cov_mu_stats = self.cov_stats / self.n_stats
     
   #    
   # def precomputes( self, theta = None ):
@@ -91,7 +102,7 @@ class SyntheticLikelihoodModelState( BaseState ):
     
   def model_parameters(self):
     #self.precomputes()
-    return self.mu_stats, self.cov_stats, self.cov_mu_stats, self.S
+    return self.mu_stats, self.n_stats, self.sum_sq_stats, self.cov_stats, self.cov_mu_stats
   
   def loglikelihood_rand( self, M ):
     if self.loglik is None:
@@ -102,7 +113,18 @@ class SyntheticLikelihoodModelState( BaseState ):
     std_stats = self.epsilon+np.sqrt(self.cov_stats)
     std_mu_stats = np.sqrt(self.cov_mu_stats)
     for m in xrange(M):
-      mu_stats = self.mu_stats + std_mu_stats*np.random.randn()
+      if self.hierarchy == "jeffreys":
+        cov_stats = wishart_rnd( self.n_stats-1, self.sum_sq_stats )
+        #cov_stats = 
+        std_stats = self.epsilon+np.sqrt(cov_stats)
+        std_mu_stats = np.sqrt(cov_stats/self.n_stats)
+        mu_stats = self.mu_stats + std_mu_stats*np.random.randn()
+        #print "cov_stats", self.cov_stats, "  vs   ", cov_stats
+        #pdb.set_trace()
+      elif self.hierarchy == "just_gaussian":
+        mu_stats = self.mu_stats + std_mu_stats*np.random.randn()
+      else:
+        assert False, "no other type yet"
       logliks[m] = self.loglikelihood_under_model(stats, mu_stats, std_stats)
     return logliks
     
@@ -112,7 +134,7 @@ class SyntheticLikelihoodModelState( BaseState ):
   def loglikelihood( self, theta = None ):
     if len(self.stats) == 0:
       self.acquire( self.S )
-      self.update_model()
+      #self.update_model()
       self.loglik = self.loglikelihood_under_model( self.obs_statistics, self.mu_stats, self.epsilon+np.sqrt(self.cov_stats) )
 
     return self.loglik